2faf95c8aa123c7a2a19c99658cadaffa39b8808
ditoma95 <devgtfod@gmail.com>
2025-08-10 07:11:05 +0000

    image

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com"
       },
+      {
+        protocol: "https",
+        hostname: "bo7de8fejcc7v3gm.public.blob.vercel-storage.com"
+      },
     ],
   },
 };
